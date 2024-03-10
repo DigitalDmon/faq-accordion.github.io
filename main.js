8de6c84e4cdc9610a6e7a321bf7095a628052c9b
DigitalDmon <161183806+DigitalDmon@users.noreply.github.com>
2024-03-10 05:24:21 -0500

    Terminado
    Archivos HTML, CSS y JS, falta un pulido respecto a la imagen de fondo, por lo demas bien.

--- main.js
+++ main.js
@@ -3,6 +3,5 @@ const boxs = document.querySelectorAll(".box");
 boxs.forEach((box) => {
     box.addEventListener("click", () => {
         box.classList.toggle("active")
-        const answerOpen = this.next
     });
 });
